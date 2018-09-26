6424705126a4c8414f5bc02df50c9e80d502d7b9
Adriano Margarin <adriano.margarin@gmail.com>
2018-09-26 12:36:42 -0300

    Removido JS que não precisa

--- javascript.js
+++ javascript.js
@@ -21,16 +21,16 @@ $(document).ready(function() {
           }, 1000);
     });
 
-    $('#buscar_caminho').on('click', function(e) {
-        e.preventdefault()
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
+    // $('#buscar_caminho').on('click', function(e) {
+    //     e.preventdefault()
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentPosition(function(position) {
 
-            var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            calcRoute(point);
-            });
-        }
-    });
+    //         var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //         calcRoute(point);
+    //         });
+    //     }
+    // });
 
     $('.show-info').on('click',function(e){
         e.preventDefault();
@@ -46,67 +46,65 @@ $(document).ready(function() {
         $(this).parent('span').children('.hide-info-desc').hide("slow");
     });
 
-    //map
-    var directionsDisplay;
-    var directionsService = new google.maps.DirectionsService();
-    var map;
-    var ftec = new google.maps.LatLng(-29.1738524, -51.2208893);
-    directionsDisplay = new google.maps.DirectionsRenderer();
-    var mapOptions = {
-      zoom:15,
-      center: ftec,
-      streetViewControl: false,
-      panControl: true,
-      overviewMapControl: true,
-      zoomControl: true,
-      scaleControl: true
-    }
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('directions-panel'));
-    var marker = new google.maps.Marker({
-                  position: ftec,
-                  map: map,
-                  title:"Uniftec - Auditório"
-    });
+    // //map
+    // var directionsDisplay;
+    // var directionsService = new google.maps.DirectionsService();
+    // var map;
+    // var ftec = new google.maps.LatLng(-29.1738524, -51.2208893);
+    // directionsDisplay = new google.maps.DirectionsRenderer();
+    // var mapOptions = {
+    //   zoom:15,
+    //   center: ftec,
+    //   streetViewControl: false,
+    //   panControl: true,
+    //   overviewMapControl: true,
+    //   zoomControl: true,
+    //   scaleControl: true
+    // }
+    // map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    // directionsDisplay = new google.maps.DirectionsRenderer();
+    // directionsDisplay.setMap(map);
+    // directionsDisplay.setPanel(document.getElementById('directions-panel'));
+    // var marker = new google.maps.Marker({
+    //               position: ftec,
+    //               map: map,
+    //               title:"Uniftec - Auditório"
+    // });
 
-    function calcRoute(starte) {
-      var start = document.getElementById("search-route").value;
-      if(starte != undefined) {
-        start = starte;
-      }
-      var end = ftec;
-      var request = {
-          origin:start,
-          destination:end,
-          travelMode: google.maps.TravelMode.DRIVING
-      };
-      directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-      }
-      });
-    }
+    // function calcRoute(starte) {
+    //   var start = document.getElementById("search-route").value;
+    //   if(starte != undefined) {
+    //     start = starte;
+    //   }
+    //   var end = ftec;
+    //   var request = {
+    //       origin:start,
+    //       destination:end,
+    //       travelMode: google.maps.TravelMode.DRIVING
+    //   };
+    //   directionsService.route(request, function(response, status) {
+    //   if (status == google.maps.DirectionsStatus.OK) {
+    //     directionsDisplay.setDirections(response);
+    //   }
+    //   });
+    // }
 
-
-
-    autocomplete = new google.maps.places.Autocomplete(
-        (document.getElementById('search-route')),
-        { types: ['geocode'] });
-    // When the user selects an address from the dropdown,
-    // populate the address fields in the form.
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      calcRoute();
-    });
+    // autocomplete = new google.maps.places.Autocomplete(
+    //     (document.getElementById('search-route')),
+    //     { types: ['geocode'] });
+    // // When the user selects an address from the dropdown,
+    // // populate the address fields in the form.
+    // google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    //   calcRoute();
+    // });
 
 
-    document.getElementById("search-route").addEventListener("keypress", function(e){
-        if (e.keyCode == 13) {
-            calcRoute();
-            return false;
-        }
-    });
+    // document.getElementById("search-route").addEventListener("keypress", function(e){
+    //     if (e.keyCode == 13) {
+    //         calcRoute();
+    //         return false;
+    //     }
+    // });
 
     $('body').scrollspy({ target: '#main-navbar' });
 
